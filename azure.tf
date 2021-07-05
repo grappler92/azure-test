f37067cce3eb50ab778088dffc7db7ffb262c0b5
Gavin Purchase <gavin.purchase@loblaw.ca>
2021-07-05 09:57:12 -0400

    initial code upload

--- azure.tf
+++ azure.tf
@@ -19,21 +19,21 @@ resource "azurerm_virtual_network" "network" {
 resource "azurerm_subnet" "snet-1" {
 	name 					= "subnet-1"
 	resource_group_name 	= azurerm_resource_group.network.name
-	virtual_network_name 	= zurerm_virtual_network.network.name
+	virtual_network_name 	= azurerm_virtual_network.network.name
 	address_prefixes 		= ["10.1.0.0/24"]
 }
 
 resource "azurerm_subnet" "snet-2" {
 	name 					= "subnet-2"
 	resource_group_name 	= azurerm_resource_group.network.name
-	virtual_network_name 	= zurerm_virtual_network.network.name
+	virtual_network_name 	= azurerm_virtual_network.network.name
 	address_prefixes 		= ["10.2.0.0/24"]
 }
 
 resource "azurerm_subnet" "snet-3" {
 	name 					= "subnet-3"
 	resource_group_name 	= azurerm_resource_group.network.name
-	virtual_network_name 	= zurerm_virtual_network.network.name
+	virtual_network_name 	= azurerm_virtual_network.network.name
 	address_prefixes 		= ["10.3.0.0/24"]
 }
 
