b5ff153d668eb9208c047a33f15b1f809c5eae17
grappler92 <grappler@outlook.com>
2021-07-06 09:02:23 -0400

    dynamic nsg test

--- cac/spoke.tf
+++ cac/spoke.tf
@@ -116,7 +116,7 @@ resource "azurerm_subnet_route_table_association" "snet-3" {
 # }
 
 module "nsg-test" {
-	source 	= "app.terraform.io/app/grappler92/workspaces/terraform-azurerm-nsg"
+	source 	= "app.terraform.io/grappler92/azurerm/nsg"
 	version = "v0.3"                                                      ## Mandatory , please use the most current version
  	rg-nsg-name	= "rg-test"
 	location	= "canadacentral"
