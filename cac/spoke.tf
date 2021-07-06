198c23f1062ee96409c28c0b7df3d61eb7d6155d
Gavin Purchase <gavin.purchase@loblaw.ca>
2021-07-06 08:25:47 -0400

    dynamic nsg test

--- cac/spoke.tf
+++ cac/spoke.tf
@@ -116,8 +116,8 @@ resource "azurerm_subnet_route_table_association" "snet-3" {
 # }
 
 module "nsg-test" {
-	source 	= "app.terraform.io/app/grappler92/workspaces/terraform-azurerm-nsg"
-	version = "v0.2"                                                      ## Mandatory , please use the most current version
+	source 	= "git::https://github.com/app.terraform.io/app/grappler92/workspaces/terraform-azurerm-nsg/?ref=v0.2"
+	# version = "v0.2"                                                      ## Mandatory , please use the most current version
  	rules = {
     	test-rule-1 = {                                       ## Mandatory Name of the Firewall Rule
       	name          		= "test01"     ## Mandatory Description of FW Rule
